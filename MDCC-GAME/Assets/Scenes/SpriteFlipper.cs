641a3414f526d69065d45b2eab3f640d88914f81
caleblaroche14 <69857383+caleblaroche14@users.noreply.github.com>
2020-09-08 16:43:46 -0400

    adding player movement

---
+++ MDCC-GAME/Assets/Scenes/SpriteFlipper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipper : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+
+    private void Awake()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // This function is called by Unity every frame the component is enabled
+    private void Update()
+    {
+        // if the A key was pressed this frame
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // if the variable isn't empty (we have a reference to our SpriteRenderer
+            if (mySpriteRenderer != null)
+            {
+                // flip the sprite
+                mySpriteRenderer.flipX = false;
+            }
+        }
+        // if the A key was pressed this frame
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // if the variable isn't empty (we have a reference to our SpriteRenderer
+            if (mySpriteRenderer != null)
+            {
+                // flip the sprite
+                mySpriteRenderer.flipX = true;
+            }
+        }
+    }
+}
+
